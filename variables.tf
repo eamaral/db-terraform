c47c874299653418b89ba54e8f9984f873c3d566
Erik <erik.fernandes87@gmail.com>
2025-03-29 18:55:56 -0300

    workflow dispatch

--- variables.tf
+++ variables.tf
@@ -1,29 +1,35 @@
 variable "region" {
   description = "Região da AWS"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "ID da VPC onde o banco será criado"
   type        = string
+  default     = ""
 }
 
 variable "private_subnets" {
   description = "Subnets privadas para o RDS"
   type        = list(string)
+  default     = []
 }
 
 variable "ecs_service_sg_id" {
   description = "Security group do ECS que acessará o banco"
   type        = string
+  default     = ""
 }
 
 variable "db_username" {
   description = "Usuário do banco"
   type        = string
+  default     = "admin"
 }
 
 variable "db_password" {
   description = "Senha do banco"
   type        = string
+  default     = "Senha123"
 }
